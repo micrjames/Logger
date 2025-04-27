467831019c9f1904db90e26e446c2ce3d55b228c
Michael James <micrjames921@gmail.com>
2025-04-26 18:36:05 -0700

    completed 'Logging messages' test block'

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 describe("A Logger", () => {
    let logger: Logger;
+   let logSpy: jest.SpyInstance;
    beforeEach(() => {
 	   logger = new Logger('info'); // Initialize logger with default log level
    });
@@ -12,13 +13,48 @@ describe("A Logger", () => {
 	   jest.clearAllMocks();
    });
    describe("Logging Messages", () => {
-	  test.todo("Should log 'info' messages.");
-	  test.todo("Should log 'error' messages.");
-	  test.todo("Should log 'debug' messages.");
-	  test.todo("Should log 'warn' messages.");
-	  test.todo("Should log 'verbose' messages.");
-	  test.todo("Should log 'silly' messages.");
-	  test.todo("Should log with metadata.");
+	  test("Should log 'info' messages.", () => {
+		  logSpy = jest.spyOn(logger['logger'], 'info');
+		  logger.log('info', 'This is an info message');
+		  expect(logSpy).toHaveBeenCalledWith('This is an info message', { meta: undefined });
+		  logSpy.mockRestore();
+	  });
+	  test("Should log 'error' messages.", () => {
+		  logSpy = jest.spyOn(logger['logger'], 'error');
+		  logger.log('error', 'This is an error message');
+		  expect(logSpy).toHaveBeenCalledWith('This is an error message', { meta: undefined });
+		  logSpy.mockRestore();
+	  });
+	  test("Should log 'debug' messages.", () => {
+		  logSpy = jest.spyOn(logger['logger'], 'debug');
+		  logger.log('debug', 'This is an debug message');
+		  expect(logSpy).toHaveBeenCalledWith('This is an debug message', { meta: undefined });
+		  logSpy.mockRestore();
+	  });
+	  test("Should log 'warn' messages.", () => {
+		  logSpy = jest.spyOn(logger['logger'], 'warn');
+		  logger.log('warn', 'This is a warn message');
+		  expect(logSpy).toHaveBeenCalledWith('This is a warn message', { meta: undefined });
+		  logSpy.mockRestore();
+	  });
+	  test("Should log 'verbose' messages.", () => {
+		  logSpy = jest.spyOn(logger['logger'], 'verbose');
+		  logger.log('verbose', 'This is a verbose message');
+		  expect(logSpy).toHaveBeenCalledWith('This is a verbose message', { meta: undefined });
+		  logSpy.mockRestore();
+	  });
+	  test("Should log 'silly' messages.", () => {
+		  logSpy = jest.spyOn(logger['logger'], 'silly');
+		  logger.log('silly', 'This is a silly message');
+		  expect(logSpy).toHaveBeenCalledWith('This is a silly message', { meta: undefined });
+		  logSpy.mockRestore();
+	  });
+	  test("Should log with metadata.", () => {
+		  logSpy = jest.spyOn(logger['logger'], 'info');
+		  logger.log('info', 'This is an info message', {userId: 123});
+		  expect(logSpy).toHaveBeenCalledWith('This is an info message', { meta: {userId: 123} });
+		  logSpy.mockRestore();
+	  });
    });
    describe("Log Level Management", () => {
 	   test.todo("Should set log level dynamically.");
